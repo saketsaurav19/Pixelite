2d01fb6d56b2743cb3f69c14ae72792d4ef550b4
saketsaurav19 <saketsaurav19@users.noreply.github.com>
2026-07-02 15:54:15 +0000

    deploy: cc1a96b00c6a27561d928ca17b6134160e9cac60

--- assets/BufferResource-dXT9730G.js
+++ assets/BufferResource-dXT9730G.js
@@ -1,4 +1,4 @@
-import{w as U,b as g,c as S,E as I,u as h}from"./index-BDoDyU8g.js";import{S as c}from"./RenderTargetSystem-Dii1B920.js";import{B as _,a as m}from"./Geometry-CkybI4p0.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,b as g,c as S,E as I,u as h}from"./index-BSaw7Rvy.js";import{S as c}from"./RenderTargetSystem-Ch6e8hq2.js";import{B as _,a as m}from"./Geometry-BIPm-5W2.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
